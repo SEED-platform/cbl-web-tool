6b4bd7a3552d089d725140d15a249bef5b9083a8
Roger-Braun <jbraun@nrel.gov>
2024-07-26 12:49:42 -0600

    merged data between API data and user data

--- flask-app/utils/merge_dicts.py
+++ flask-app/utils/merge_dicts.py
@@ -8,5 +8,23 @@ def merge_dicts(dict1, dict2):
             merged_dict["street_address"] = value
         else:
             merged_dict[key.lower()] = value
+
+    merged_dict = remove_duplicate_vals(merged_dict)     
     
     return merged_dict
+
+# removes different keys that have the same value
+def remove_duplicate_vals(d):
+    value_to_keys = {}
+    for key, value in d.items():
+        if value not in value_to_keys:
+            value_to_keys[value] = [key]
+        else:
+            value_to_keys[value].append(key)
+
+    new_dict = {}
+    for value, keys in value_to_keys.items():
+        if keys:
+            new_dict[keys[0]] = value
+    
+    return new_dict
