f97f12c26401839e641a3d20482af57fd118ca0c
Mian <rahmanakbarmian@gmail.com>
2024-08-16 16:42:10 -0600

    merge lint changes

--- angular-app/src/app/mapbox-map/mapbox-map.component.ts
+++ angular-app/src/app/mapbox-map/mapbox-map.component.ts
@@ -1,11 +1,5 @@
-import { CommonModule } from '@angular/common';
 import type { OnDestroy, OnInit } from '@angular/core';
 import { ChangeDetectorRef, Component } from '@angular/core';
-import MapboxDraw from '@mapbox/mapbox-gl-draw';
-import * as mapboxgl from 'mapbox-gl';
-import type { Subscription } from 'rxjs';
-import { v4 as uuidv4 } from 'uuid';
-import { environment } from '../../environments/environment';
 import { GeoJsonService } from '../services/geojson.service';
 import { FlaskRequests } from '../services/server.service';
 import * as mapboxgl from 'mapbox-gl';
