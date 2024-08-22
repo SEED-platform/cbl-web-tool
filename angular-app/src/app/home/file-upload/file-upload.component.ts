0277054db03447ebc8a0bb0d1fee4b10ee2a48fc
Roger-Braun <jbraun@nrel.gov>
2024-08-22 14:56:16 -0400

    better file checking

--- angular-app/src/app/home/file-upload/file-upload.component.ts
+++ angular-app/src/app/home/file-upload/file-upload.component.ts
@@ -144,12 +144,12 @@ export class FileUploadComponent {
 
     this.apiHandler.sendInitialData(fileData).subscribe(
       (response) => {
+        console.log("made it bruh")
         console.log(response.message); // Handle successful response
         this.initialJsonData = response.user_data;
         sessionStorage.setItem('FIRSTTABLEDATA', LZString.compress(this.initialJsonData));
-        if (this.userFile) {
-          this.router.navigate(['/first-table']);
-        }
+        this.router.navigate(['/first-table']);
+        
       },
       (errorResponse) => {
         console.log(errorResponse.error.message); // Handle error response
