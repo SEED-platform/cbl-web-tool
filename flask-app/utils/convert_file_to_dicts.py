f11ec69e68a6e9edc6dc0ab88d23719343a52ae9
Devin Brady <dbrady@nrel.gov>
2025-04-02 16:49:39 -0600

    Remove try-except blocks

--- flask-app/utils/convert_file_to_dicts.py
+++ flask-app/utils/convert_file_to_dicts.py
@@ -6,53 +6,39 @@ from utils.location_error import LocationError
 
 
 # NOTE: When converting to a data_frame, duplicate columns will be renamed (i.e Address and Address
-# will become Address and Address.1) SO, the json file and the resulting
+# will become Address and Address.1) So, the json file and the resulting
 # dictionary may have keys like Address and Address.1
 def convert_file_to_dicts(file):
+    """
+    Convert a file into a series of dicts, depending on the file type
+
+    File types are checked here: angular-app/src/app/home/file-upload/file-upload.component.ts
+    An error is displayed to the user if they attempt to upload a file of a different type.
+    """
     file_type = file.content_type
 
-    newError = LocationError("Failed to read file.")
-
     if file_type == "application/json":
-        try:
-            file_content = file.read().decode("utf-8")
-            file_data = json.loads(file_content)
-        except Exception:
-            return newError
+        file_content = file.read().decode("utf-8")
+        file_data = json.loads(file_content)
 
-        return file_data
+    elif file_type == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
+        data_frame = pd.read_excel(file)
+        json_data = data_frame.to_json(orient="records")
+        file_data = json.loads(json_data)
 
-    if file_type == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
-        try:
-            data_frame = pd.read_excel(file)
-            json_data = data_frame.to_json(orient="records")
-            file_data = json.loads(json_data)
-        except Exception:
-            return newError
+    elif file_type in {"application/csv", "text/csv"}:
+        data_frame = pd.read_csv(file)
+        json_data = data_frame.to_json(orient="records")
+        file_data = json.loads(json_data)
 
-        return file_data
-
-    if file_type in {"application/csv", "text/csv"}:
-        try:
-            data_frame = pd.read_csv(file)
-            json_data = data_frame.to_json(orient="records")
-            file_data = json.loads(json_data)
-        except Exception:
-            return newError
-
-        return file_data
-
-    if file_type in {"application/geo+json", "application/octet-stream"}:
-        try:
-            file_content = file.read().decode("utf-8")
-            file_data = json.loads(file_content)
-        except Exception:
-            return newError
+    elif file_type in {"application/geo+json", "application/octet-stream"}:
+        file_content = file.read().decode("utf-8")
+        file_data = json.loads(file_content)
 
         # need to extract info from geoJSON to make regular dict object
         file_data = convert_geojson_to_dict(file_data)
 
-        return file_data
+    return file_data
 
 
 def convert_geojson_to_dict(file_data):
